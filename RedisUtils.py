ecf29f880e8563ec87e6ed64cd38829626dc43b2
ubuntu <ubuntu@localhost.localdomain>
2025-03-11 14:45:22 +0800

    增加Redis相关

---
+++ RedisUtils.py
@@ -0,0 +1,29 @@
+import redis
+
+"""
+返回错误码和Redis连接
+
+"""
+def RedisConnect(host='localhost', port=6379, db_num = 0):
+   # 创建 Redis 连接
+    r = redis.Redis(
+        host=host,
+        port=port,
+        db=db_num,
+        decode_responses=True  # 自动解码二进制数据为字符串
+    )
+    try:
+        r.ping()
+        return r, 0
+        print("Successfully connected to Redis!")
+    except redis.ConnectionError:
+        print("Failed to connect to Redis")
+        return r, 0
+
+
+if __name__ == "__main__":
+    # 测试写入和读取
+    redis_conn, _ = RedisConnect()
+    redis_conn.set('foo', 'bar')
+    value = redis_conn.get('foo')
+    print(value)  # 输出: 'bar'
